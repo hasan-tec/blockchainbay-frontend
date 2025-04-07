d2465b34921955f17e42f25c130ecc1abc56c435
hasan-tec <hasan.anas.yt@gmail.com>
2025-04-07 21:11:53 +0500

    changed the loading logo

--- app/media-kit/loading.tsx
+++ app/media-kit/loading.tsx
@@ -1,23 +1,83 @@
-export default function MediaKitLoading() {
-    return (
-      <div className="min-h-screen bg-[#07071C] flex items-center justify-center">
-        <div className="flex flex-col items-center">
-          <div className="relative w-16 h-16 mb-8">
-            <div className="absolute inset-0 bg-gradient-to-tr from-[#F7984A] to-[#F7984A]/80 rounded-lg blur-[4px] animate-pulse"></div>
-            <div className="absolute inset-1 bg-[#F7984A] rounded-lg flex items-center justify-center">
-              <svg width="32" height="32" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M12 2L20 7V17L12 22L4 17V7L12 2Z" fill="white" />
-              </svg>
-            </div>
+export default function Loading() {
+  return (
+    <div className="min-h-screen bg-[#07071C] flex items-center justify-center">
+      <div className="flex flex-col items-center">
+        <div className="relative w-24 h-24 mb-8">
+          <div className="absolute inset-0 blur-[8px] animate-pulse">
+            <svg width="100%" height="100%" viewBox="0 0 923 923" xmlns="http://www.w3.org/2000/svg">
+              <defs>
+                <linearGradient id="linear-gradient" x1="0" y1="923" x2="923" y2="0" gradientUnits="userSpaceOnUse">
+                  <stop offset="0" stopColor="#0c066a"/>
+                  <stop offset="1" stopColor="#48448f"/>
+                </linearGradient>
+                <linearGradient id="linear-gradient-2" x1="214.03" y1="728.58" x2="760.19" y2="361.18" gradientUnits="userSpaceOnUse">
+                  <stop offset="0" stopColor="#ef4b3d"/>
+                  <stop offset="1" stopColor="#f7984a"/>
+                </linearGradient>
+                <linearGradient id="linear-gradient-3" x1="65.66" y1="497.45" x2="528.59" y2="34.52" gradientUnits="userSpaceOnUse">
+                  <stop offset="0" stopColor="#ef4b3d"/>
+                  <stop offset="1" stopColor="#f7984a"/>
+                </linearGradient>
+                <linearGradient id="linear-gradient-4" x1="225.37" y1="374.61" x2="345.77" y2="254.21" href="#linear-gradient-3"/>
+                <linearGradient id="linear-gradient-5" x1="185.49" y1="681.23" x2="657.14" y2="209.57" href="#linear-gradient-3"/>
+                <linearGradient id="linear-gradient-6" x1="455.35" y1="603.3" x2="722.76" y2="335.89" href="#linear-gradient-3"/>
+                <linearGradient id="linear-gradient-7" x1="113.81" y1="577.4" x2="677.84" y2="236.82" gradientUnits="userSpaceOnUse">
+                  <stop offset=".04" stopColor="#ef4b3d"/>
+                  <stop offset="1" stopColor="#f7984a"/>
+                </linearGradient>
+              </defs>
+              <path fill="url(#linear-gradient)" d="m923,923H0V0h923v923Z"/>
+              <g>
+                <path fill="url(#linear-gradient-2)" d="m580.22,482.17c-.32.27-.64.53-.96.8.32-.27.64-.53.96-.8Z"/>
+                <path fill="url(#linear-gradient-3)" d="m239.76,323.47c-.34.27-.68.53-1.02.8.34-.27.68-.53,1.02-.8Z"/>
+                <path fill="url(#linear-gradient-4)" d="m276.58,324.27c-.31-.27-.62-.54-.93-.81.31.27.62.54.93.81Z"/>
+                <path fill="url(#linear-gradient-5)" d="m543.28,323.46c-.3.28-.6.57-.9.85.3-.28.6-.57.9-.85Z"/>
+                <path fill="url(#linear-gradient-6)" d="m709.43,454.31c22.18-11.29,36.05-31.16,32.81-64.28-4.36-45.26-43.47-60.43-92.84-64.75v-62.77h-38.26v61.12c-10.08,0-20.34.2-30.55.4v-61.52s-38.23,0-38.23,0v62.76c-8.29.17-16.42.33-24.37.33v-.19l-52.76-.02v40.8s28.26-.53,27.79-.02c15.49.01,20.55,9,22,16.75v71.51s.01,100.45.01,100.45c-.68,4.87-3.55,12.65-14.38,12.66.49.43-27.83,0-27.83,0l-7.59,45.63h49.77c9.27.01,18.39.15,27.33.23l.03,63.48h38.21s-.01-62.81-.01-62.81c10.51.21,20.66.31,30.57.29v62.53s38.24,0,38.24,0l.02-63.38c64.3-3.69,109.31-19.87,114.91-80.2,4.51-48.57-18.37-70.25-54.88-79.01Zm-127.93-85.36c21.57,0,89.42-6.86,89.43,38.16,0,43.17-67.84,38.13-89.43,38.13v-76.29Zm-.02,198.92l.02-84.12c25.92,0,107.22-7.45,107.23,42.04.01,47.46-81.31,42.06-107.25,42.08Z"/>
+                <path fill="url(#linear-gradient-7)" d="m405.72,454.31c22.18-11.29,36.05-31.16,32.81-64.28-4.36-45.26-43.47-60.43-92.84-64.75v-62.77h-38.26v61.12c-10.08,0-20.34.2-30.55.4v-61.52s-38.23,0-38.23,0v62.76c-8.29.17-16.42.33-24.37.33v-.19l-52.76-.02v40.8s28.26-.53,27.79-.02c15.49.01,20.55,9,22,16.75v71.51s.01,100.45.01,100.45c-.68,4.87-3.55,12.65-14.38,12.66.49.43-27.83,0-27.83,0l-7.59,45.63h49.77c9.27.01,18.39.15,27.33.23l.03,63.48h38.21s-.01-62.81-.01-62.81c10.51.21,20.66.31,30.57.29v62.53s38.24,0,38.24,0l.02-63.38c64.3-3.69,109.31-19.87,114.91-80.2,4.51-48.57-18.37-70.25-54.88-79.01Zm-127.93-85.36c21.57,0,89.42-6.86,89.43,38.16,0,43.17-67.84,38.13-89.43,38.13v-76.29Zm-.02,198.92l.02-84.12c25.92,0,107.22-7.45,107.23,42.04.01,47.46-81.31,42.06-107.25,42.08Z"/>
+              </g>
+            </svg>
           </div>
-          <div className="flex space-x-2">
-            <div className="w-3 h-3 rounded-full bg-[#F7984A] animate-bounce" style={{ animationDelay: "0ms" }}></div>
-            <div className="w-3 h-3 rounded-full bg-[#F7984A] animate-bounce" style={{ animationDelay: "150ms" }}></div>
-            <div className="w-3 h-3 rounded-full bg-[#F7984A] animate-bounce" style={{ animationDelay: "300ms" }}></div>
+          <div className="absolute inset-0">
+            <svg width="100%" height="100%" viewBox="0 0 923 923" xmlns="http://www.w3.org/2000/svg">
+              <defs>
+                <linearGradient id="linear-gradient-a" x1="0" y1="923" x2="923" y2="0" gradientUnits="userSpaceOnUse">
+                  <stop offset="0" stopColor="#0c066a"/>
+                  <stop offset="1" stopColor="#48448f"/>
+                </linearGradient>
+                <linearGradient id="linear-gradient-b" x1="214.03" y1="728.58" x2="760.19" y2="361.18" gradientUnits="userSpaceOnUse">
+                  <stop offset="0" stopColor="#ef4b3d"/>
+                  <stop offset="1" stopColor="#f7984a"/>
+                </linearGradient>
+                <linearGradient id="linear-gradient-c" x1="65.66" y1="497.45" x2="528.59" y2="34.52" gradientUnits="userSpaceOnUse">
+                  <stop offset="0" stopColor="#ef4b3d"/>
+                  <stop offset="1" stopColor="#f7984a"/>
+                </linearGradient>
+                <linearGradient id="linear-gradient-d" x1="225.37" y1="374.61" x2="345.77" y2="254.21" href="#linear-gradient-c"/>
+                <linearGradient id="linear-gradient-e" x1="185.49" y1="681.23" x2="657.14" y2="209.57" href="#linear-gradient-c"/>
+                <linearGradient id="linear-gradient-f" x1="455.35" y1="603.3" x2="722.76" y2="335.89" href="#linear-gradient-c"/>
+                <linearGradient id="linear-gradient-g" x1="113.81" y1="577.4" x2="677.84" y2="236.82" gradientUnits="userSpaceOnUse">
+                  <stop offset=".04" stopColor="#ef4b3d"/>
+                  <stop offset="1" stopColor="#f7984a"/>
+                </linearGradient>
+              </defs>
+              <path fill="url(#linear-gradient-a)" d="m923,923H0V0h923v923Z"/>
+              <g>
+                <path fill="url(#linear-gradient-b)" d="m580.22,482.17c-.32.27-.64.53-.96.8.32-.27.64-.53.96-.8Z"/>
+                <path fill="url(#linear-gradient-c)" d="m239.76,323.47c-.34.27-.68.53-1.02.8.34-.27.68-.53,1.02-.8Z"/>
+                <path fill="url(#linear-gradient-d)" d="m276.58,324.27c-.31-.27-.62-.54-.93-.81.31.27.62.54.93.81Z"/>
+                <path fill="url(#linear-gradient-e)" d="m543.28,323.46c-.3.28-.6.57-.9.85.3-.28.6-.57.9-.85Z"/>
+                <path fill="url(#linear-gradient-f)" d="m709.43,454.31c22.18-11.29,36.05-31.16,32.81-64.28-4.36-45.26-43.47-60.43-92.84-64.75v-62.77h-38.26v61.12c-10.08,0-20.34.2-30.55.4v-61.52s-38.23,0-38.23,0v62.76c-8.29.17-16.42.33-24.37.33v-.19l-52.76-.02v40.8s28.26-.53,27.79-.02c15.49.01,20.55,9,22,16.75v71.51s.01,100.45.01,100.45c-.68,4.87-3.55,12.65-14.38,12.66.49.43-27.83,0-27.83,0l-7.59,45.63h49.77c9.27.01,18.39.15,27.33.23l.03,63.48h38.21s-.01-62.81-.01-62.81c10.51.21,20.66.31,30.57.29v62.53s38.24,0,38.24,0l.02-63.38c64.3-3.69,109.31-19.87,114.91-80.2,4.51-48.57-18.37-70.25-54.88-79.01Zm-127.93-85.36c21.57,0,89.42-6.86,89.43,38.16,0,43.17-67.84,38.13-89.43,38.13v-76.29Zm-.02,198.92l.02-84.12c25.92,0,107.22-7.45,107.23,42.04.01,47.46-81.31,42.06-107.25,42.08Z"/>
+                <path fill="url(#linear-gradient-g)" d="m405.72,454.31c22.18-11.29,36.05-31.16,32.81-64.28-4.36-45.26-43.47-60.43-92.84-64.75v-62.77h-38.26v61.12c-10.08,0-20.34.2-30.55.4v-61.52s-38.23,0-38.23,0v62.76c-8.29.17-16.42.33-24.37.33v-.19l-52.76-.02v40.8s28.26-.53,27.79-.02c15.49.01,20.55,9,22,16.75v71.51s.01,100.45.01,100.45c-.68,4.87-3.55,12.65-14.38,12.66.49.43-27.83,0-27.83,0l-7.59,45.63h49.77c9.27.01,18.39.15,27.33.23l.03,63.48h38.21s-.01-62.81-.01-62.81c10.51.21,20.66.31,30.57.29v62.53s38.24,0,38.24,0l.02-63.38c64.3-3.69,109.31-19.87,114.91-80.2,4.51-48.57-18.37-70.25-54.88-79.01Zm-127.93-85.36c21.57,0,89.42-6.86,89.43,38.16,0,43.17-67.84,38.13-89.43,38.13v-76.29Zm-.02,198.92l.02-84.12c25.92,0,107.22-7.45,107.23,42.04.01,47.46-81.31,42.06-107.25,42.08Z"/>
+              </g>
+            </svg>
           </div>
         </div>
+        <div className="flex space-x-2">
+          <div className="w-3 h-3 rounded-full bg-[#F7984A] animate-bounce" style={{ animationDelay: "0ms" }}></div>
+          <div className="w-3 h-3 rounded-full bg-[#F7984A] animate-bounce" style={{ animationDelay: "150ms" }}></div>
+          <div className="w-3 h-3 rounded-full bg-[#F7984A] animate-bounce" style={{ animationDelay: "300ms" }}></div>
+        </div>
       </div>
-    )
-  }
-  
-  
+    </div>
+  )
+}
